b4288acf6330f3636a2cca9bf9092f86d413e350
arhamrahim97 <moh.arham.rahim@gmail.com>
2022-05-25 00:20:03 +0800

    bug_spinner

--- resources/views/dashboard/pages/guest/register.blade.php
+++ resources/views/dashboard/pages/guest/register.blade.php
@@ -238,6 +238,7 @@
                 $(document).on('select2:open', () => {
                     document.querySelector('.select2-search__field').focus();
                 });
+
                 $(function() {
                     $('.modal').modal({
                         backdrop: 'static',
