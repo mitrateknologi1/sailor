1522a8dcd17c078f898b858a88392081d436f67f
syahruncega@gmail.com <syahruncega@gmail.com>
2022-07-09 13:59:26 +0800

    API Controller Anggota Keluarga

--- app/Http/Controllers/api/ApiAnggotaKeluargaController.php
+++ app/Http/Controllers/api/ApiAnggotaKeluargaController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\AnggotaKeluarga;
+use App\Models\Pemberitahuan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ApiAnggotaKeluargaController extends Controller
 {
@@ -36,10 +39,29 @@ class ApiAnggotaKeluargaController extends Controller
      */
     public function store(Request $request)
     {
-        $reqBody = json_decode($request->getContent());
-        if (is_array($reqBody) && sizeof($reqBody) > 0) {
-        } else {
-        }
+        $request->validate([
+            "bidan_id" => 'required|exists:bidan,id',
+            "kartu_keluarga_id" => 'required|exists:kartu_keluarga,id',
+            "user_id" => 'exists:users,id',
+            "nama_lengkap" => 'required|string',
+            "nik" => 'required|numeric|unique:anggota_keluarga,nik',
+            "jenis_kelamin" => 'required|in:PEREMPUAN,LAKI-LAKI',
+            "tempat_lahir" => 'required|string',
+            "tanggal_lahir" => 'required|string',
+            "agama_id" => 'required|exists:agama,id',
+            "pendidikan_id" => 'required|exists:pendidikan,id',
+            "jenis_pekerjaan_id" => 'required|exists:pekerjaan,id',
+            "golongan_darah_id" => 'required|exists:golongan_darah,id',
+            "status_perkawinan_id" => 'required|exists:status_perkawinan,id',
+            "status_hubungan_dalam_keluarga_id" => 'required|exists:status_hubungan,id',
+            "kewarganegaraan" => 'required|string',
+            "nama_ayah" => 'required|string',
+            "nama_ibu" => 'required|string',
+            "is_valid" => 'required|numeric|in:0,1',
+            "tanggal_validasi" => 'required|string',
+        ]);
+
+        return AnggotaKeluarga::create($request->all());
     }
 
     /**
@@ -48,9 +70,13 @@ class ApiAnggotaKeluargaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
-        //
+        $relation = $request->relation;
+        if ($relation) {
+            return AnggotaKeluarga::with('provinsi', 'kabupatenKota', 'kecamatan', 'desaKelurahan', 'bidan')->where('id', $id)->first();
+        }
+        return AnggotaKeluarga::where('id', $id)->first();
     }
 
     /**
@@ -62,7 +88,30 @@ class ApiAnggotaKeluargaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $anggotaKeluarga = AnggotaKeluarga::find($id);
+        $request->validate([
+            "bidan_id" => 'exists:bidan,id',
+            "kartu_keluarga_id" => 'exists:kartu_keluarga,id',
+            "user_id" => 'exists:users,id',
+            "nama_lengkap" => 'string',
+            "nik" => "numeric|unique:anggota_keluarga,nik,$id",
+            "jenis_kelamin" => 'in:PEREMPUAN,LAKI-LAKI',
+            "tempat_lahir" => 'string',
+            "tanggal_lahir" => 'string',
+            "agama_id" => 'exists:agama,id',
+            "pendidikan_id" => 'exists:pendidikan,id',
+            "jenis_pekerjaan_id" => 'exists:pekerjaan,id',
+            "golongan_darah_id" => 'exists:golongan_darah,id',
+            "status_perkawinan_id" => 'exists:status_perkawinan,id',
+            "status_hubungan_dalam_keluarga_id" => 'exists:status_hubungan,id',
+            "kewarganegaraan" => 'string',
+            "nama_ayah" => 'string',
+            "nama_ibu" => 'string',
+            "is_valid" => 'numeric|in:0,1',
+            "tanggal_validasi" => 'string',
+        ]);
+        $anggotaKeluarga->update($request->all());
+        return $anggotaKeluarga;
     }
 
     /**
@@ -73,6 +122,35 @@ class ApiAnggotaKeluargaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $anggotaKeluarga = AnggotaKeluarga::find($id);
+
+        if ((Auth::user()->profil->id == $anggotaKeluarga->bidan_id) || (Auth::user()->role == 'admin')) {
+            if (Storage::exists('upload/foto_profil/keluarga/' . $anggotaKeluarga->foto_profil)) {
+                Storage::delete('upload/foto_profil/keluarga/' . $anggotaKeluarga->foto_profil);
+            }
+
+            if ($anggotaKeluarga->wilayahDomisili) {
+                if (Storage::exists('upload/surat_keterangan_domisili/' . $anggotaKeluarga->wilayahDomisili->file_ket_domisili)) {
+                    Storage::delete('upload/surat_keterangan_domisili/' . $anggotaKeluarga->wilayahDomisili->file_ket_domisili);
+                }
+                $anggotaKeluarga->wilayahDomisili->delete();
+            }
+
+            $pemberitahuan = Pemberitahuan::where('anggota_keluarga_id', $anggotaKeluarga->id);
+
+            if ($pemberitahuan) {
+                $pemberitahuan->delete();
+            }
+
+
+            if ($anggotaKeluarga->user) {
+                $anggotaKeluarga->user->delete();
+            }
+            return $anggotaKeluarga->delete();
+        }
+
+        return response([
+            'message' => "Not Permitted",
+        ], 403);
     }
 }
